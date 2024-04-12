01c24d0189788fb740a652125f77af6c4f8efe16
BellTitiporn <139092027+BellTitiporn@users.noreply.github.com>
2024-04-12 14:19:41 +0700

    Update App.jsx

--- navbar_res/src/App.jsx
+++ navbar_res/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./component/Home";
 import Navbar from "./component/Navbar";
 import About from "./component/About";
+import Favorite from "./component/Favorite";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/favorite" element={<Favorite />} />
         </Routes>
       </main>
     </Router>
