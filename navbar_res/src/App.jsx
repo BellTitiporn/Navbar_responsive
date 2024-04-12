815bf6b1fe8c8f58b101bdc82f96de4a792e15ff
BellTitiporn <139092027+BellTitiporn@users.noreply.github.com>
2024-04-12 10:50:31 +0700

    Update App.jsx

--- navbar_res/src/App.jsx
+++ navbar_res/src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./component/Home";
+import Navbar from "./component/Navbar";
+import About from "./component/About";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <Router>
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
